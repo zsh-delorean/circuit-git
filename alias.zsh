d6bcb7d8245cd233bbe3cddc528d4eecbb563846
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-10 08:04:05 -0500

    Do not set ignore-submodules for short status

--- alias.zsh
+++ alias.zsh
@@ -234,7 +234,7 @@ alias gSx='git-submodule-remove'
 # Working Copy (w)
 #
 
-alias gws='git status --ignore-submodules=${_git_status_ignore_submodules} --short'
+alias gws='git status --short'
 alias gwS='git status --ignore-submodules=${_git_status_ignore_submodules}'
 alias gwd='git diff --no-ext-diff'
 alias gwD='git diff --no-ext-diff --word-diff'
